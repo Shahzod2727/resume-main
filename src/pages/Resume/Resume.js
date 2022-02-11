dea9d73e78c819ac15ad49e1ef777fee19cf3aa4
Shahzod2727 <shahzodjava@gmail.com>
2022-02-11 17:15:22 +0500

    part 10

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import WorkIcon from "@mui/icons-material/Work";
+import SchoolIcon from '@mui/icons-material/School';
 import resumeDate from "../../utils/resumeDate";
-import "./Resume.css";
+// import experiencesData from '../../utils/experiencesData'
+
+import CustomTimeline, {
+  CustomTimeLineSeparator,
+} from "../../components/Timeline/Timeline";
+
 import { Typography } from "@mui/material";
+import TimelineItem from "@mui/lab/TimelineItem";
+
+import TimelineContent from "@mui/lab/TimelineContent";
+import "./Resume.css";
+
 const Resume = () => {
   return (
     // xs, extra-small: 0px.
@@ -14,21 +26,82 @@ const Resume = () => {
     <>
       {/* About me */}
       <Grid container className="section pb_45">
-        <Grid item className="section_title mb_45">
+        <Grid item className="section_title mb_30">
           <span></span>
           <h6 variant="h6" className="section_title_text">
             About Me
           </h6>
         </Grid>
-        <Grid item>
-          
-          <Typography xs={12} variant='body2' className="aboutme_text" variant="h6">
+        <Grid item xs={12}>
+          <Typography variant="body2" className="aboutme_text" variant="h6">
             {resumeDate.about}
           </Typography>
         </Grid>
       </Grid>
       {/* Education and experiences */}
-      <Grid container className="section"></Grid>
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 variant="h6" className="section_title_text">
+            Resume
+          </h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container className="resume_timeline">
+            {/* Experiences */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title={"Work Experience"} icon={<WorkIcon />}>
+                {resumeDate.experiences &&
+                  resumeDate.experiences.map((experience) => (
+                    <TimelineItem>
+                      <CustomTimeLineSeparator />
+                      <TimelineContent className="timeline_content">
+                        <Typography className="timeline_title">
+                          {experience.title}
+                        </Typography>
+                        <Typography variant="caption" className="timeline_date">
+                          {experience.date}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          className="timeline_description"
+                        >
+                          {experience.description}
+                        </Typography>
+                      </TimelineContent>
+                    </TimelineItem>
+                  ))}
+              </CustomTimeline>
+            </Grid>
+
+            {/* Education */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title={"Education"} icon={<SchoolIcon />}>
+                {resumeDate. educations &&
+                  resumeDate. educations.map((education) => (
+                    <TimelineItem>
+                      <CustomTimeLineSeparator />
+                      <TimelineContent className="timeline_content">
+                        <Typography className="timeline_title">
+                          {education.title}
+                        </Typography>
+                        <Typography variant="caption" className="timeline_date">
+                          {education.date}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          className="timeline_description"
+                        >
+                          {education.description}
+                        </Typography>
+                      </TimelineContent>
+                    </TimelineItem>
+                  ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
 
       {/* Services */}
       <Grid container className="section"></Grid>
--- src/utils/resumeDate.js
+++ src/utils/resumeDate.js
@@ -1,40 +1,81 @@
-import React from 'react';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import React from "react";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 export default {
-    name:'Shahzod Habibov',
-    title:'Full stack developer',
+  name: "Shahzod Habibov",
+  title: "Full stack developer",
 
-    birthday:"08th July 1999",
-    email:"shahzodjava@gmail.com",
-    address:"Buxoro Vobkent Rozmoz(QFY)",
-    phone:'+99891 2401743',
+  birthday: "08th July 1999",
+  email: "shahzodjava@gmail.com",
+  address: "Buxoro Vobkent Rozmoz(QFY)",
+  phone: "+99891 2401743",
 
-    socials:{
-        Facebook:{
-            link:"https://www.facebook.com",
-            text:"ShahzodIsCoding",
-            icon:<FacebookIcon/>
-        },
-        Twitter:{
-            link:"https://www.twitter.com",
-            text:"MyTwitter",
-            icon:<TwitterIcon/>
-        },
-        LinkedIn:{
-            link:"https://www.linkedin.com",
-            text:"MyLinkedin",
-            icon:<LinkedInIcon/>
-        },
-        GitHub:{
-            link:"https://www.github.com",
-            text:"MyGithub",
-            icon:<GitHubIcon/>
-        },
+  socials: {
+    Facebook: {
+      link: "https://www.facebook.com",
+      text: "ShahzodIsCoding",
+      icon: <FacebookIcon />,
     },
-    about: "Assalamu alaykum! I am Asadbek Azamov, born on May 12, 1999. I live in Kyzyltepa district, Navoi region, Republic of Uzbekistan. I am a student of Tashkent University of Information Technologies. Since 2019, I have been dealing with the frontend part of Web programming. My goal is to contribute to the development of IT in our country.Set yourself a huge goal and don't stop moving until it reaches the goal. We will only achieve the goal when we deserve it. And for that, we need to start the action today, right now. Mistakes will be the foundation for us to be strong!"
+    Twitter: {
+      link: "https://www.twitter.com",
+      text: "MyTwitter",
+      icon: <TwitterIcon />,
+    },
+    LinkedIn: {
+      link: "https://www.linkedin.com",
+      text: "MyLinkedin",
+      icon: <LinkedInIcon />,
+    },
+    GitHub: {
+      link: "https://www.github.com",
+      text: "MyGithub",
+      icon: <GitHubIcon />,
+    },
+  },
+  about:
+    "Assalamu alaykum! I am Asadbek Azamov, born on May 12, 1999. I live in Kyzyltepa district, Navoi region, Republic of Uzbekistan. I am a student of Tashkent University of Information Technologies. Since 2019, I have been dealing with the frontend part of Web programming. My goal is to contribute to the development of IT in our country.",
+  experiences: [
+    {
+      title: "work 1",
+      date: "2018 - Present",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+    {
+      title: "work 2",
+      date: "2018 - 2015",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+    {
+      title: "work 3",
+      date: "Aug 2016 - Sept 2018",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+  ],
 
-}
+  educations: [
+    {
+      title: "Education 1",
+      date: "2018 - Present",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+    {
+      title: "Education 2",
+      date: "2018 - 2015",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+    {
+      title: "Education 3",
+      date: "Aug 2016 - Sept 2018",
+      description:
+        "React programmer at the ‘Agro-Industrial Digitization Center’",
+    },
+  ],
+};
